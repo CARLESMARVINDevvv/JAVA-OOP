38f381554e37208c299bc5b744aa6ba2da66df0a
CARLES MARVIN <charlesmarvin4321@gmail.com>
2025-06-18 22:18:54 +0700

    enkapsulasi  dalam OPP

---
+++ Encapsulasi.java
@@ -0,0 +1,35 @@
+package com.tutorial;
+
+class Data {
+    public int intPublic;
+    private int intPrivate;
+
+    public Data() {
+        this.intPublic = 0;
+        this.intPrivate = 0;
+    }
+
+    // Getter untuk intPrivate
+    public int getIntPrivate() {
+        return intPrivate;
+    }
+
+    // Setter untuk intPrivate
+    public void setIntPrivate(int value) {
+        this.intPrivate = value;
+    }
+}
+
+public class Encapsulasi {
+    public static void main(String[] args) {
+        Data object = new Data();
+
+        // public (read & write langsung)
+        object.intPublic = 10;
+        System.out.println("Nilai intPublic: " + object.intPublic);
+
+        // private (pakai setter untuk mengubah, getter untuk membaca)
+        object.setIntPrivate(99); // Mengatur nilai
+        System.out.println("Nilai intPrivate: " + object.getIntPrivate()); // Membaca nilai
+    }
+}
